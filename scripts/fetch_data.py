020351b0ea972079ef10af1ee68848b511575f59
HalT <harryturnerbath@gmail.com>
2026-06-29 21:32:16 +0100

    Add NESO demand API call

--- scripts/fetch_data.py
+++ scripts/fetch_data.py
@@ -6,7 +6,6 @@ import pandas as pd
 import requests
 from datetime import datetime as dt, timezone, timedelta
 from pathlib import Path
-import urllib3 as lib3
 
 # ===============================
 # system paths
@@ -21,14 +20,14 @@ output_dir.mkdir(parents=True, exist_ok=True)
 # ===============================
 # API request - generation data
 url = 'https://data.elexon.co.uk/bmrs/api/v1/generation/outturn/summary'
-end_date = dt.now(timezone.utc)
+today = dt.now(timezone.utc)
 time_lag = timedelta(days=7)
-start_date = pd.Timestamp(end_date - time_lag).round('D').strftime('%Y-%m-%d %H:%M:%S')
-end_date = pd.Timestamp(end_date).strftime('%Y-%m-%d %H:%M:%S')
+start_date = pd.Timestamp(today - time_lag).round('D').strftime('%Y-%m-%d %H:%M:%S')
+today = pd.Timestamp(today).strftime('%Y-%m-%d %H:%M:%S')
 
 params = {
     'startTime' : start_date,
-    'endTime' : end_date
+    'endTime' : today
 }
 
 response = requests.get(url, params = params)
@@ -50,23 +49,80 @@ gen = pd.json_normalize(
 gen['settlementPeriod'] = pd.to_numeric(gen['settlementPeriod'])
 gen['startTime'] = pd.to_datetime(gen['startTime'], utc=True)
 
+# Pivot - generation data
+gen = gen.pivot_table(
+    index=['startTime', 'settlementPeriod'],
+    columns='fuelType',
+    values='generation'
+).reset_index().rename_axis(None, axis=1)
+
+
 # ===============================
 # 2 - NESO demand data
 # ===============================
 # API request - NESO demand data
-url = 'https://api.neso.energy/api/3/action/datastore_search?resource_id=177f6fa4-ae49-4182-81ea-0c6b35f26ca6'
+url_start = 'https://api.neso.energy'
+url_end = '/api/3/action/datastore_search?resource_id=8a4a771c-3929-4e56-93ad-cdf13219dea5'
 
-response = requests.get(url)
 
-if response.status_code == 200:
-    print('NESO demand API call successful!')
-else:
-    raise Exception(f"NESO demand API call not successful. Status code: {response.status_code}")
+today = dt.now(timezone.utc)
+start_date = pd.to_datetime('2026-01-01', utc=True)
 
-dmnd = response.json()
+dmnd = []
 
-dmnd = dmnd['result']['records']
+i = 1
 
-dmnd = pd.json_normalize(dmnd)
+while start_date <= today:
+    url = url_start + url_end
 
-dmnd = pd.DataFrame(dmnd)
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        print('NESO demand API call successful!')
+    else:
+        raise Exception(f"NESO demand API call not successful. Status code: {response.status_code}")
+
+    dmnd_resp = response.json()
+    records = dmnd_resp['result']['records']
+    
+    if not records:
+        print("Reached the end of available records. Stopping.")
+        break
+
+    dmnd.extend(records)
+
+    start_date = pd.to_datetime(dmnd_resp['result']['records'][-1]['SETTLEMENT_DATE'], utc=True)
+    url_end = dmnd_resp['result']['_links']['next']
+
+    print(f'Loop run:{i} times. Collected data up to {start_date}.')
+
+    i += 1
+
+    print(url)
+
+# No flattening required!
+df_dmnd = pd.DataFrame(dmnd)
+
+# View the result
+print(df_dmnd.tail())
+
+df_dmnd['SETTLEMENT_DATE'] = pd.to_datetime(df_dmnd['SETTLEMENT_DATE'], utc=True)
+
+
+
+#     raise Exception(f"NESO demand API call not successful. Status code: {response.status_code}")
+
+
+
+# dmnd = dmnd['result']['records']
+
+# dmnd = pd.json_normalize(dmnd)
+
+# dmnd = dmnd.loc[:, ['SETTLEMENT_DATE', 'SETTLEMENT_PERIOD', 'EMBEDDED_SOLAR_GENERATION', 'EMBEDDED_WIND_GENERATION']]
+# # dmnd['SETTLEMENT_DATE']
+
+# print(gen.dtypes)
+# print(dmnd.dtypes)
+
+# jn.display(gen)
+# # print(list(dmnd.columns.values))
